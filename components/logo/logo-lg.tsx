9b4b82e7a939131e266f3d622926d07835a2c934
Badr OUAHBI <badii_00@hotmail.com>
2023-03-22 22:50:54 +0100

    asset folder move - Link

--- components/logo/logo-lg.tsx
+++ components/logo/logo-lg.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 export const LogoLg = () => {
   return (
       <div className="m-3 p-3">
-      <a className="flex flex-row" href="/">
-        <img className="login-logo-image h-20 pr-5" src="@/asset/img/logo-wippi.jpg" alt="logo-image" />
-        <div className="logo-text pt-2">
-          <h1 className="text-4xl font-bold">WIPPI</h1>
-          <h2 className="text-lg">GLOBAL-BIM</h2>
-        </div>
-      </a>
+      <Link className="flex flex-row" href="/">
+        <img className="login-logo-image h-20 pr-5" src="./asset/img/logo-wippi.jpg" alt="logo-image" />
+          <div className="logo-text pt-2">
+            <h1 className="text-4xl font-bold">WIPPI</h1>
+            <h2 className="text-lg">GLOBAL-BIM</h2>
+          </div>
+      </Link>
     </div>
   )
 };
