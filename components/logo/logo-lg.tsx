b009d0591ba812fa4c41efa7e6a2f862f0990e9b
Badr OUAHBI <badii_00@hotmail.com>
2023-04-09 21:30:27 +0200

    Dynamic logo name

--- components/logo/logo-lg.tsx
+++ components/logo/logo-lg.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
+import { useLanguageContext } from "@/middleware";
+import { Dictionary } from "@/interfaces";
+
 export const LogoLg = () => {
+  const t:Dictionary = useLanguageContext()[1];
+
   return (
       <div className="m-3 p-3">
       <Link className="flex flex-row" href="/">
         <img className="login-logo-image h-20 pr-5" src="../asset/img/logo-wippi.jpg" alt="logo-image" />
           <div className="logo-text pt-2">
-            <h1 className="text-4xl font-bold">WIPPI</h1>
-            <h2 className="text-lg">GLOBAL-BIM</h2>
+            <h1 className="text-4xl font-bold">{t.appName}</h1>
+            <h2 className="text-lg">{t.BIMPlatform}</h2>
           </div>
       </Link>
     </div>
