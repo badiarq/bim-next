c9361095c5b5d1878d82ffb0e89a5c88caad8fcf
Badr OUAHBI <badii_00@hotmail.com>
2023-04-10 02:15:23 +0200

    Auto stash before checking out "HEAD"

--- middleware/authenticator.tsx
+++ middleware/authenticator.tsx
@@ -4,11 +4,13 @@ import { useRouter } from "next/router";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { toast } from 'react-toastify';
 
-import { useAppContext } from "@/middleware/context-provider";
+import { useAppContext, useLanguageContext } from "@/middleware";
+// import { Dictionary } from "@/interfaces";
 
 let authInitialized = false;
 
 export const Authenticator: FC = () => {
+  const router = useRouter();
   const auth = getAuth();
   const dispatch = useAppContext()[1];
   const router = useRouter();
@@ -17,15 +19,22 @@ export const Authenticator: FC = () => {
     toast.success(`${userName}, welcome !`)
   }
 
+
+  // const notifyLogin = (userName : String | null, welcome:string, appName:string) => {
+  //   toast.success(`${userName}, ${welcome} ${appName} !`)
+  // }
+
   const listenToAuthChanges = () => {
     onAuthStateChanged(auth, (foundUser) => {
       const user = foundUser ? { ...foundUser } : null;
       dispatch({ type: "UPDATE_USER", payload: user });
+
       if(user) {
         const displayName = user.displayName;
         if  (user && user.displayName && user.displayName.trim() !== "") {
           router.push('/map');
-          notifyLogin(displayName);
+          // const t:Dictionary = useLanguageContext()[1];
+          // notifyLogin(displayName, t.welcomeTo, t.appName);
         }
       }
     });
