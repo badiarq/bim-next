c7eb7654a360ae40721189c818bb2764ac69fcd0
Badr OUAHBI <badii_00@hotmail.com>
2023-04-10 20:51:12 +0200

    google logo fix

--- components/user/google-button.tsx
+++ components/user/google-button.tsx
@@ -14,8 +14,8 @@ export const GoogleLogin: FC = () => {
 
   return (
     <>
-      <div id="googleButotn" className='btn ml-0 mt-3 border-2 dark:border-none dark:bg-white p-2 primary-dark flex flex-row justify-center w-full h-10 items-center'>
-        <img className="mr-3" src="./asset/icon/google-logo.svg" alt='google icon' width="24" height="24"/>
+      <div id="googleButotn" className='btn ml-0 mt-3 border-2 border-dark dark:border-none bg-white p-2 primary-dark flex flex-row justify-center w-full h-10 items-center'>
+        <img className="mr-3" src="../asset/icon/google-logo.svg" alt='google icon' width="24" height="24"/>
         <button
           className="text-dark"
           onClick={onLoginClick}
