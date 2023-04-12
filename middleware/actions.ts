3c4e944d5943eadb616c9ea9a36953b78c88866e
Badr OUAHBI <badii_00@hotmail.com>
2023-04-12 23:57:17 +0200

    map and building-viewer state.user

--- middleware/actions.ts
+++ middleware/actions.ts
@@ -1,4 +1,4 @@
-export type ActionType = "LOGIN" | "LOGOUT" | "UPDATE_USER";
+export type ActionType = "LOGIN" | "LOGOUT" | "UPDATE_USER" | "START_MAP" | "REMOVE_MAP";
 
 export interface Action {
   type: ActionType;
--- pages/map/index.tsx
+++ pages/map/index.tsx
@@ -1,10 +1,35 @@
+import { useRef, useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { useLanguageContext, useAppContext } from '@/middleware';
 import { MapLayout } from "@/components/layouts"
-import { useLanguageContext } from '@/middleware';
 import { Dictionary } from "@/interfaces";
 
 export default function MapPage() {
 
+  const router = useRouter();
+  const [state, dispatch] = useAppContext();
+  const canvasRef = useRef(null);
+
   const t:Dictionary = useLanguageContext()[1];
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if(canvas && state.user) {
+      dispatch({type: "START_MAP", payload: canvas});
+    }
+
+    return () => {
+      dispatch({type: "REMOVE_MAP"});
+    }
+
+  }, []);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined' && !state.user) {
+      router.push('/login');
+    }
+  }, [router, state.user]);
   
   return (
     <MapLayout
